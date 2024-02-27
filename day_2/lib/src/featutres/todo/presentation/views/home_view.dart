37b51a5d0914edd7c524fb79fcf5012cb4a39104
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-02-27 19:43:33 +0800

    day_2 commit

---
+++ day_2/lib/src/featutres/todo/presentation/views/home_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  DateTime today = DateTime.now();
+
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to 100 Days of Flutter'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const Text('Calendar View Package'),
+            Text('Selected Day = ${today.toString().split(' ')[0]}'),
+            TableCalendar(
+              locale: "en_US",
+              rowHeight: 43,
+              headerStyle: const HeaderStyle(
+                formatButtonVisible: false,
+                titleCentered: true,
+              ),
+              availableGestures: AvailableGestures.all,
+              selectedDayPredicate: (day) => isSameDay(day, today),
+              focusedDay: today,
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              onDaySelected: _onDaySelected,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
