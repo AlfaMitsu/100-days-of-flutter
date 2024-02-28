b467b4fb6fa439a6b170d010be6afe411bdee16d
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-02-29 00:38:42 +0800

    flutter slidable

--- day_6/lib/src/featutres/todo/presentation/views/home_view.dart
+++ day_6/lib/src/featutres/todo/presentation/views/home_view.dart
@@ -1,13 +1,101 @@
-import 'package:day_4/src/featutres/todo/presentation/widgets/pie_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
-class HomeView extends StatelessWidget {
+import '../../../../constants/colors.dart';
+
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: MyPieChart(),
+    return Scaffold(
+      body: ListView(
+        children: [
+          Slidable(
+            startActionPane: ActionPane(
+              motion: const StretchMotion(),
+              children: [
+                SlidableAction(
+                  onPressed: ((context) {}),
+                  backgroundColor: Colors.green,
+                  icon: Icons.phone,
+                ),
+                SlidableAction(
+                  flex: 2,
+                  onPressed: ((context) {}),
+                  backgroundColor: kBlueColor,
+                  icon: Icons.chat,
+                ),
+              ],
+            ),
+            endActionPane: ActionPane(
+              motion: const StretchMotion(),
+              children: [
+                SlidableAction(
+                  onPressed: ((context) {}),
+                  backgroundColor: Colors.red,
+                  icon: Icons.delete,
+                ),
+              ],
+            ),
+            child: Container(
+              color: kGrey300Color,
+              child: const ListTile(
+                title: Text('Trishia Buenne'),
+                subtitle: Text('09123456789'),
+                leading: Icon(
+                  Icons.person,
+                  size: 40,
+                ),
+              ),
+            ),
+          ),
+          Slidable(
+            startActionPane: ActionPane(
+              motion: const StretchMotion(),
+              children: [
+                SlidableAction(
+                  onPressed: ((context) {}),
+                  backgroundColor: Colors.green,
+                  icon: Icons.phone,
+                ),
+                SlidableAction(
+                  flex: 2,
+                  onPressed: ((context) {}),
+                  backgroundColor: kBlueColor,
+                  icon: Icons.chat,
+                ),
+              ],
+            ),
+            endActionPane: ActionPane(
+              motion: const StretchMotion(),
+              children: [
+                SlidableAction(
+                  onPressed: ((context) {}),
+                  backgroundColor: Colors.red,
+                  icon: Icons.delete,
+                ),
+              ],
+            ),
+            child: Container(
+              color: kGrey300Color,
+              child: const ListTile(
+                title: Text('Al-Fasheer'),
+                subtitle: Text('09987654321'),
+                leading: Icon(
+                  Icons.person,
+                  size: 40,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
