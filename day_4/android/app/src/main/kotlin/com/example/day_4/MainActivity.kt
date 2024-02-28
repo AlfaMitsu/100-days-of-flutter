cc106efbd5107d30f10d40d4cbeae0f9a6a155dc
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-02-28 22:48:03 +0800

    pie_chart

---
+++ day_4/android/app/src/main/kotlin/com/example/day_4/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.day_4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ day_5/android/app/src/main/kotlin/com/example/day_5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.day_5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
