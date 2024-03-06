54de9eddc683e93efce3bba7848caf2f77580346
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-03-07 01:14:34 +0800

    lottie

---
+++ day_22/lib/src/featutres/todo/presentation/views/home_view.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _controller.dispose();
+  }
+
+  bool bookmarked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            if (bookmarked == false) {
+              bookmarked = true;
+              _controller.forward();
+            } else {
+              bookmarked = false;
+              _controller.reverse();
+            }
+          },
+          child: Lottie.network(
+            'https://assets5.lottiefiles.com/packages/lf20_xdfeea13.json',
+            controller: _controller,
+          ),
+        ),
+      ),
+    );
+  }
+}
