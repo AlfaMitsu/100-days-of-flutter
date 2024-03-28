2d5ccb28cf7fb8e208e04d2b69dea9384c7db979
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-03-28 23:16:56 +0800

    day 45 update

---
+++ day_45_copy/lib/src/features/todo/data/data_sources/arrays.dart
@@ -0,0 +1,71 @@
+List topPicksArr = [
+  {
+    "name": "The Dissapearance of Emila Zola",
+    "author": "Michael Rosen",
+    "img": "lib/src/resources/assets/images/1.jpg"
+  },
+  {
+    "name": "Fatherhood",
+    "author": "Marcus Berkmann",
+    "img": "lib/src/resources/assets/images/2.jpg"
+  },
+  {
+    "name": "The Time Travellers Handbook",
+    "author": "Stride Lottie",
+    "img": "lib/src/resources/assets/images/3.jpg"
+  }
+];
+
+List bestArr = [
+  {
+    "name": "Fatherhood",
+    "author": "by Christopher Wilson",
+    "img": "lib/src/resources/assets/images/4.jpg",
+    "rating": 5.0
+  },
+  {
+    "name": "In A Land Of Paper Gods",
+    "author": "by Rebecca Mackenzie",
+    "img": "lib/src/resources/assets/images/5.jpg",
+    "rating": 4.0
+  },
+  {
+    "name": "Tattletale",
+    "author": "by Sarah J. Noughton",
+    "img": "lib/src/resources/assets/images/6.jpg",
+    "rating": 3.0
+  }
+];
+
+List genresArr = [
+  {
+    "name": "Graphic Novels",
+    "img": "lib/src/resources/assets/images/g1.png",
+  },
+  {
+    "name": "Graphic Novels",
+    "img": "lib/src/resources/assets/images/g1.png",
+  },
+  {
+    "name": "Graphic Novels",
+    "img": "lib/src/resources/assets/images/g1.png",
+  }
+];
+
+List recentArr = [
+  {
+    "name": "The Fatal Tree",
+    "author": "by Jake Arnott",
+    "img": "lib/src/resources/assets/images/10.jpg"
+  },
+  {
+    "name": "Day Four",
+    "author": "by LOTZ, SARAH",
+    "img": "lib/src/resources/assets/images/11.jpg"
+  },
+  {
+    "name": "Door to Door",
+    "author": "by Edward Humes",
+    "img": "lib/src/resources/assets/images/12.jpg"
+  }
+];
--- day_45_copy/lib/src/features/todo/presentation/views/home_view.dart
+++ day_45_copy/lib/src/features/todo/presentation/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
+import '../../data/data_sources/arrays.dart';
 import '../widgets/best_seller_cell.dart';
 import '../widgets/color_extension.dart';
 import '../widgets/genres_cell.dart';
@@ -22,78 +23,6 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   TextEditingController txtName = TextEditingController();
   TextEditingController txtEmail = TextEditingController();
-
-  List topPicksArr = [
-    {
-      "name": "The Dissapearance of Emila Zola",
-      "author": "Michael Rosen",
-      "img": "lib/src/resources/assets/images/1.jpg"
-    },
-    {
-      "name": "Fatherhood",
-      "author": "Marcus Berkmann",
-      "img": "lib/src/resources/assets/images/2.jpg"
-    },
-    {
-      "name": "The Time Travellers Handbook",
-      "author": "Stride Lottie",
-      "img": "lib/src/resources/assets/images/3.jpg"
-    }
-  ];
-
-  List bestArr = [
-    {
-      "name": "Fatherhood",
-      "author": "by Christopher Wilson",
-      "img": "lib/src/resources/assets/images/4.jpg",
-      "rating": 5.0
-    },
-    {
-      "name": "In A Land Of Paper Gods",
-      "author": "by Rebecca Mackenzie",
-      "img": "lib/src/resources/assets/images/5.jpg",
-      "rating": 4.0
-    },
-    {
-      "name": "Tattletale",
-      "author": "by Sarah J. Noughton",
-      "img": "lib/src/resources/assets/images/6.jpg",
-      "rating": 3.0
-    }
-  ];
-
-  List genresArr = [
-    {
-      "name": "Graphic Novels",
-      "img": "lib/src/resources/assets/images/g1.png",
-    },
-    {
-      "name": "Graphic Novels",
-      "img": "lib/src/resources/assets/images/g1.png",
-    },
-    {
-      "name": "Graphic Novels",
-      "img": "lib/src/resources/assets/images/g1.png",
-    }
-  ];
-
-  List recentArr = [
-    {
-      "name": "The Fatal Tree",
-      "author": "by Jake Arnott",
-      "img": "lib/src/resources/assets/images/10.jpg"
-    },
-    {
-      "name": "Day Four",
-      "author": "by LOTZ, SARAH",
-      "img": "lib/src/resources/assets/images/11.jpg"
-    },
-    {
-      "name": "Door to Door",
-      "author": "by Edward Humes",
-      "img": "lib/src/resources/assets/images/12.jpg"
-    }
-  ];
 
   @override
   Widget build(BuildContext context) {
