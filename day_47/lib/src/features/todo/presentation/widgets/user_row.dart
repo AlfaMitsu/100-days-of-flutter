b9530739f7ebf2298d80577aa9435ba0892cc2a8
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-04-02 19:23:11 +0800

    day 47 update

--- day_47/lib/src/features/todo/presentation/widgets/user_row.dart
+++ day_47/lib/src/features/todo/presentation/widgets/user_row.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
-import 'color_extension.dart';
+import '../../../../constants/styles.dart';
 
 class UserRow extends StatelessWidget {
   final Map uObj;
-  const UserRow({super.key, required this.uObj});
+
+  const UserRow({
+    super.key,
+    required this.uObj,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -15,41 +19,30 @@ class UserRow extends StatelessWidget {
           ClipRRect(
             borderRadius: BorderRadius.circular(20),
             child: Image.asset(
-              uObj["image"] as String? ?? "",
+              uObj['image'] as String? ?? '',
               width: 40,
               height: 40,
               fit: BoxFit.cover,
             ),
           ),
-          const SizedBox(
-            width: 8,
-          ),
+          const SizedBox(width: 8),
           Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  uObj["name"] as String? ?? "",
+                  uObj['name'] as String? ?? '',
                   textAlign: TextAlign.left,
                   maxLines: 1,
-                  style: TextStyle(
-                    color: TColor.primaryText,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                  ),
+                  style: kChatViewActiveTextStyle,
                 ),
-                const SizedBox(
-                  height: 2,
-                ),
+                const SizedBox(height: 2),
                 Text(
-                  uObj["tag"] as String? ?? "",
+                  uObj['tag'] as String? ?? '',
                   textAlign: TextAlign.left,
                   maxLines: 1,
-                  style: TextStyle(
-                    color: TColor.secondaryText,
-                    fontSize: 14,
-                  ),
+                  style: kHomeRowTextStyle,
                 ),
               ],
             ),
