4fd2a82756d10fd78d4c7b315da05d46c1162694
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-04-02 19:19:49 +0800

    day 47 update

--- day_47/lib/src/features/todo/presentation/widgets/user_notification_row.dart
+++ day_47/lib/src/features/todo/presentation/widgets/user_notification_row.dart
@@ -1,66 +1,61 @@
+import 'package:day_47/src/constants/assets.dart';
 import 'package:flutter/material.dart';
 
-import 'color_extension.dart';
+import '../../../../constants/styles.dart';
 
 class UserNotificationRow extends StatelessWidget {
   final Map nObj;
-  const UserNotificationRow({super.key, required this.nObj});
+
+  const UserNotificationRow({
+    super.key,
+    required this.nObj,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      padding: const EdgeInsets.symmetric(vertical: 4),
       child: Row(
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(20),
             child: Image.asset(
-              nObj["image"] as String? ?? "",
+              nObj['image'] as String? ?? '',
               width: 40,
               height: 40,
               fit: BoxFit.cover,
             ),
           ),
-          const SizedBox(
-            width: 8,
-          ),
+          const SizedBox(width: 8),
           Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  nObj["name"] as String? ?? "",
+                  nObj['name'] as String? ?? '',
                   textAlign: TextAlign.left,
                   maxLines: 1,
-                  style: TextStyle(
-                    color: TColor.primaryText,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                  ),
+                  style: kChatViewActiveTextStyle,
                 ),
-                const SizedBox(
-                  height: 2,
-                ),
+                const SizedBox(height: 2),
                 Text(
-                  "${nObj["message"] as String? ?? ""} | ${nObj["time"] as String? ?? ""} ",
+                  '${nObj['message'] as String? ?? ''} | ${nObj['time'] as String? ?? ''} ',
                   textAlign: TextAlign.left,
                   maxLines: 1,
-                  style: TextStyle(
-                    color: TColor.secondaryText,
-                    fontSize: 14,
-                  ),
+                  style: kHomeRowTextStyle,
                 ),
               ],
             ),
           ),
           IconButton(
-              onPressed: () {},
-              icon: Image.asset(
-                "lib/src/resources/assets/images/more.png",
-                width: 20,
-                height: 20,
-              ))
+            onPressed: () {},
+            icon: Image.asset(
+              kImgMoreButton,
+              width: 20,
+              height: 20,
+            ),
+          ),
         ],
       ),
     );
