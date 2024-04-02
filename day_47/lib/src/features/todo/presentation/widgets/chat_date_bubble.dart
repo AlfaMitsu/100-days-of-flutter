fe2f167b5b2fde8df29b0e2f776dafd3b7ed240c
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-04-02 18:52:07 +0800

    day 47 update

--- day_47/lib/src/features/todo/presentation/widgets/chat_date_bubble.dart
+++ day_47/lib/src/features/todo/presentation/widgets/chat_date_bubble.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'color_extension.dart';
+import '../../../../constants/colors.dart';
+import '../../../../constants/styles.dart';
 
 class ChatDateBubble extends StatelessWidget {
   final String date;
@@ -15,24 +16,20 @@ class ChatDateBubble extends StatelessWidget {
           Expanded(
             child: Container(
               height: 0.5,
-              color: TColor.primaryText.withOpacity(0.8),
+              color: kPrimaryText.withOpacity(0.8),
             ),
           ),
-          const SizedBox(
-            width: 15,
-          ),
+          const SizedBox(width: 15),
           Text(
             date,
             textAlign: TextAlign.center,
-            style: TextStyle(color: TColor.secondaryText, fontSize: 13),
+            style: kCommentViewTextStyle,
           ),
-          const SizedBox(
-            width: 15,
-          ),
+          const SizedBox(width: 15),
           Expanded(
             child: Container(
               height: 0.5,
-              color: TColor.primaryText.withOpacity(0.8),
+              color: kPrimaryText.withOpacity(0.8),
             ),
           ),
         ],
