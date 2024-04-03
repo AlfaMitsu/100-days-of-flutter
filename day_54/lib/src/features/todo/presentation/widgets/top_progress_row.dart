0ede90229802e04c2f9a15e698bf0e21ed29f0d2
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-04-04 05:25:24 +0800

    day 54 update

--- day_54/lib/src/features/todo/presentation/widgets/top_progress_row.dart
+++ day_54/lib/src/features/todo/presentation/widgets/top_progress_row.dart
@@ -19,44 +19,57 @@ class TopProgressRow extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: isActivePadding
-          ? const EdgeInsets.only(bottom: 10, top: 0)
-          : const EdgeInsets.only(bottom: 0, top: 10),
+          ? const EdgeInsets.only(
+              bottom: 10,
+              top: 0,
+            )
+          : const EdgeInsets.only(
+              bottom: 0,
+              top: 10,
+            ),
       child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+        margin: const EdgeInsets.symmetric(
+          vertical: 12,
+          horizontal: 8,
+        ),
         padding: const EdgeInsets.all(15),
         width: 150,
         decoration: BoxDecoration(
-            color: bgColor,
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                  color: bgColor.withOpacity(
-                    0.7,
-                  ),
-                  offset: const Offset(0, 4),
-                  blurRadius: 8)
-            ]),
+          color: bgColor,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+                color: bgColor.withOpacity(0.7),
+                offset: const Offset(
+                  0,
+                  4,
+                ),
+                blurRadius: 8),
+          ],
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SimpleCircularProgressBar(
               size: 60,
-              progressColors: [textColor.withOpacity(0.7)],
+              progressColors: [
+                textColor.withOpacity(0.7),
+              ],
               backStrokeWidth: 0,
-              maxValue: double.tryParse(tObj["max_val"].toString()) ?? 0.0,
+              maxValue: double.tryParse(tObj['max_val'].toString()) ?? 0,
               progressStrokeWidth: 6,
               valueNotifier:
-                  ValueNotifier(double.tryParse(tObj["val"].toString()) ?? 0.0),
+                  ValueNotifier(double.tryParse(tObj['val'].toString()) ?? 0),
               mergeMode: true,
               onGetText: (double value) {
                 TextStyle centerTextStyle = TextStyle(
-                    fontSize: 15,
-                    color: textColor.withOpacity(0.7),
-                    fontWeight: FontWeight.w700);
-
+                  fontSize: 15,
+                  color: textColor.withOpacity(0.7),
+                  fontWeight: FontWeight.w700,
+                );
                 return Text(
-                  tObj["displayType"].toString() == "val"
-                      ? '${value.toInt()}/${int.tryParse(tObj["max_val"].toString()) ?? 0}'
+                  tObj['displayType'].toString() == 'val'
+                      ? '${value.toInt()}/${int.tryParse(tObj['max_val'].toString()) ?? 0}'
                       : '${value.toInt()}%',
                   style: centerTextStyle,
                 );
@@ -64,20 +77,22 @@ class TopProgressRow extends StatelessWidget {
             ),
             const Spacer(),
             Text(
-              tObj["name"].toString(),
+              tObj['name'].toString(),
               style: TextStyle(
-                  fontSize: 15, color: textColor, fontWeight: FontWeight.w700),
+                fontSize: 15,
+                color: textColor,
+                fontWeight: FontWeight.w700,
+              ),
             ),
-            const SizedBox(
-              height: 4,
-            ),
+            const SizedBox(height: 4),
             Text(
-              tObj["sub_name"].toString(),
+              tObj['sub_name'].toString(),
               style: TextStyle(
-                  fontSize: 11,
-                  color: textColor.withOpacity(0.7),
-                  fontWeight: FontWeight.w700),
-            )
+                fontSize: 11,
+                color: textColor.withOpacity(0.7),
+                fontWeight: FontWeight.w700,
+              ),
+            ),
           ],
         ),
       ),
