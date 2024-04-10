99d2282769ede846456426f57788edd33e2366ef
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-04-10 20:45:33 +0800

    day 57 update

--- day_57/lib/src/constants/colors.dart
+++ day_57/lib/src/constants/colors.dart
@@ -2,14 +2,6 @@ import 'package:flutter/material.dart';
 
 // Brand Colors
 
-const primaryColor = Color.fromARGB(255, 246, 241, 225);
-const secondaryColor = Color(0xFF1E90FF);
-const bgColor = Color(0xFFFEFAEF);
-const elevatedButtonColor = Color(0xFF183b37);
-const favoriteIconColor = Color.fromARGB(255, 252, 220, 178);
-const splineColor = Color.fromARGB(255, 210, 163, 96);
-const secondarybgColor = Color.fromARGB(255, 238, 240, 242);
-
 // Common Colors
 
 const kWhiteColor = Color(0xFFFFFFFF);
