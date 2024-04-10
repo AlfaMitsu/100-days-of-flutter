87eece3cb5f563b044aa117b6f5a803993f92811
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-04-10 19:32:27 +0800

    day 53 update

--- day_53/lib/src/constants/assets.dart
+++ day_53/lib/src/constants/assets.dart
@@ -1,4 +1,4 @@
 const String kImgSample = 'lib/src/resources/assets/images/kImgSample.jpg';
-const String kImgProfile = 'lib/src/resources/assets/images/kImgProfile.png';
+const String kImgProfile = 'lib/src/resources/assets/images/kImgProfile.jpg';
 const String kImgWifiRouter = 'lib/src/resources/assets/images/kImgWifiRouter.png';
 const String kImgRoomSpeaker = 'lib/src/resources/assets/images/kImgRoomSpeaker.png';
