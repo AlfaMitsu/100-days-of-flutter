e55ae8842c5cf9e926de786dceaf16d3bd67bdc6
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-04-11 22:26:57 +0800

    dashboard ui

--- day_61/lib/src/features/todo/presentation/views/home_view.dart
+++ day_61/lib/src/features/todo/presentation/views/home_view.dart
@@ -1,10 +1,64 @@
+import 'package:day_61/src/constants/dimensions.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../constants/assets.dart';
+import '../../../../constants/colors.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: kWhiteColor,
+                boxShadow: [
+                  BoxShadow(
+                    color: kGrey300Color,
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Column(
+                    children: [
+                      const SizedBox(height: defaultPadding * 2),
+                      SizedBox(
+                        height: 90,
+                        width: 90,
+                        child: Stack(
+                          children: [
+                            Align(
+                              alignment: Alignment.center,
+                              child: SizedBox(
+                                height: 60,
+                                child: ClipOval(
+                                  child: Image.asset(
+                                    kImgProfile,
+                                    scale: 2.5,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
