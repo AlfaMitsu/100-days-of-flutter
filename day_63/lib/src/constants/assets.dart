9313640a0fb76216126938c2afdd8b1778bbdd24
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-04-13 19:57:02 +0800

    day 63 update

--- day_63/lib/src/constants/assets.dart
+++ day_63/lib/src/constants/assets.dart
@@ -1,22 +1,5 @@
 //images
 
 const String kImgProfile = 'lib/src/resources/assets/images/kImgProfile.jpg';
-const String kImg1 = 'lib/src/resources/assets/images/kImg1.jpg';
-const String kImg2 = 'lib/src/resources/assets/images/kImg2.jpg';
-const String kImg3 = 'lib/src/resources/assets/images/kImg3.jpg';
-const String kImg4 = 'lib/src/resources/assets/images/kImg4.jpg';
-const String kImg5 = 'lib/src/resources/assets/images/kImg5.jpg';
-const String kImgDevice1 = 'lib/src/resources/assets/images/kImgDevice1.png';
-const String kImgDevice2 = 'lib/src/resources/assets/images/kImgDevice2.png';
-const String kImgDevice3 = 'lib/src/resources/assets/images/kImgDevice3.png';
-const String kImgDevice4 = 'lib/src/resources/assets/images/kImgDevice4.png';
-const String kImgDevice5 = 'lib/src/resources/assets/images/kImgDevice5.png';
-const String kImgDevice6 = 'lib/src/resources/assets/images/kImgDevice6.png';
-const String kImgDevice7 = 'lib/src/resources/assets/images/kImgDevice7.png';
 
 //icons
-
-const String kIcHome = 'lib/src/resources/assets/icons/kIcHome.png';
-const String kIcSearch = 'lib/src/resources/assets/icons/kIcSearch.png';
-const String kIcDashboard = 'lib/src/resources/assets/icons/kIcDashboard.png';
-const String kIcAccount = 'lib/src/resources/assets/icons/kIcAccount.png';
