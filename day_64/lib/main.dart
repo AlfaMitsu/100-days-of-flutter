229471435ad51a12cfec029d1a08f7e77de05a97
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-04-16 13:11:24 +0800

    update

--- day_64/lib/main.dart
+++ day_64/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeView(title: 'Get It',),
+      home: HomeView(title: 'Get It'),
     );
   }
 }
