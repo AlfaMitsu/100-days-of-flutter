b2db62fe96b90a6fa38c131a5812150f8cf14619
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-04-23 16:31:04 +0800

    job finder ui

---
+++ day_72/lib/src/features/todo/presentation/widgets/job_carousel.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+import '../../data/models/job_model.dart';
+import 'items_jobs.dart';
+
+class JobCarousel extends StatelessWidget {
+  final List<Job> jobs;
+
+  const JobCarousel({
+    super.key,
+    required this.jobs,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      items: jobs
+          .map((e) => ItemsJobs(
+                job: e,
+                themeDark: jobs.indexOf(e) == 0,
+              ))
+          .toList(),
+      options: CarouselOptions(
+        enableInfiniteScroll: false,
+        reverse: false,
+        viewportFraction: 0.86,
+        height: 230,
+      ),
+    );
+  }
+}
