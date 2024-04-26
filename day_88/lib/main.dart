3c7b36f0c1342b2f9396916c635d95e430b3586b
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-04-26 09:14:57 +0800

    quickalert

--- day_88/lib/main.dart
+++ day_88/lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'src/features/todo/presentation/views/custom_video_controllers.dart';
 import 'src/features/todo/presentation/views/home_view.dart';
-import 'src/features/todo/presentation/views/play_video_from_asset.dart';
-import 'src/features/todo/presentation/views/play_video_from_network.dart';
-import 'src/features/todo/presentation/views/play_video_from_network_quality_urls.dart';
-import 'src/features/todo/presentation/views/play_video_from_vimeo_id.dart';
-import 'src/features/todo/presentation/views/play_video_from_vimeo_private_id.dart';
-import 'src/features/todo/presentation/views/play_video_from_youtube.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -18,19 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: {
-        '/fromVimeoId': (context) => const PlayVideoFromVimeoId(),
-        '/fromVimeoPrivateId': (context) => const PlayVideoFromVimeoPrivateId(),
-        '/fromYoutube': (context) => const PlayVideoFromYoutube(),
-        '/fromAsset': (context) => const PlayVideoFromAsset(),
-        '/fromNetwork': (context) => const PlayVideoFromNetwork(),
-        '/fromNetworkQualityUrls': (context) =>
-            const PlayVideoFromNetworkQualityUrls(),
-        '/customVideo': (context) => const CustomVideoControllers(),
-      },
-      home: const HomeView(),
+      home: HomeView(),
     );
   }
 }
--- day_88/lib/src/features/todo/presentation/views/home_view.dart
+++ day_88/lib/src/features/todo/presentation/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quickalert/quickalert.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -10,58 +11,211 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
+    final successAlert = buildButton(
+      onTap: () {
+        QuickAlert.show(
+          context: context,
+          type: QuickAlertType.success,
+          text: 'Transaction Completed Successfully!',
+          autoCloseDuration: const Duration(seconds: 2),
+          showConfirmBtn: false,
+        );
+      },
+      title: 'Success',
+      text: 'Transaction Completed Successfully!',
+      leadingImage: 'assets/success.gif',
+    );
+
+    final errorAlert = buildButton(
+      onTap: () {
+        QuickAlert.show(
+          context: context,
+          type: QuickAlertType.error,
+          title: 'Oops...',
+          text: 'Sorry, something went wrong',
+          backgroundColor: Colors.black,
+          titleColor: Colors.white,
+          textColor: Colors.white,
+        );
+      },
+      title: 'Error',
+      text: 'Sorry, something went wrong',
+      leadingImage: 'assets/error.gif',
+    );
+
+    final warningAlert = buildButton(
+      onTap: () {
+        QuickAlert.show(
+          context: context,
+          type: QuickAlertType.warning,
+          text: 'You just broke protocol',
+        );
+      },
+      title: 'Warning',
+      text: 'You just broke protocol',
+      leadingImage: 'assets/warning.gif',
+    );
+
+    final infoAlert = buildButton(
+      onTap: () {
+        QuickAlert.show(
+          context: context,
+          type: QuickAlertType.info,
+          text: 'Buy two, get one free',
+        );
+      },
+      title: 'Info',
+      text: 'Buy two, get one free',
+      leadingImage: 'assets/info.gif',
+    );
+
+    final confirmAlert = buildButton(
+      onTap: () {
+        QuickAlert.show(
+          onCancelBtnTap: () {
+            Navigator.pop(context);
+          },
+          context: context,
+          type: QuickAlertType.confirm,
+          text: 'Do you want to logout',
+          titleAlignment: TextAlign.right,
+          textAlignment: TextAlign.right,
+          confirmBtnText: 'Yes',
+          cancelBtnText: 'No',
+          confirmBtnColor: Colors.white,
+          backgroundColor: Colors.black,
+          headerBackgroundColor: Colors.grey,
+          confirmBtnTextStyle: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+          barrierColor: Colors.white,
+          titleColor: Colors.white,
+          textColor: Colors.white,
+        );
+      },
+      title: 'Confirm',
+      text: 'Do you want to logout',
+      leadingImage: 'assets/confirm.gif',
+    );
+
+    final loadingAlert = buildButton(
+      onTap: () {
+        QuickAlert.show(
+          context: context,
+          type: QuickAlertType.loading,
+          title: 'Loading',
+          text: 'Fetching your data',
+        );
+      },
+      title: 'Loading',
+      text: 'Fetching your data',
+      leadingImage: 'assets/loading.gif',
+    );
+
+    final customAlert = buildButton(
+      onTap: () {
+        var message = '';
+        QuickAlert.show(
+          context: context,
+          type: QuickAlertType.custom,
+          barrierDismissible: true,
+          confirmBtnText: 'Save',
+          customAsset: 'assets/custom.gif',
+          widget: TextFormField(
+            decoration: const InputDecoration(
+              alignLabelWithHint: true,
+              hintText: 'Enter Phone Number',
+              prefixIcon: Icon(
+                Icons.phone_outlined,
+              ),
+            ),
+            textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.phone,
+            onChanged: (value) => message = value,
+          ),
+          onConfirmBtnTap: () async {
+            if (message.length < 5) {
+              await QuickAlert.show(
+                context: context,
+                type: QuickAlertType.error,
+                text: 'Please input something',
+              );
+              return;
+            }
+            Navigator.pop(context);
+            if (mounted) {
+              QuickAlert.show(
+                context: context,
+                type: QuickAlertType.success,
+                text: "Phone number '$message' has been saved!.",
+              );
+            }
+          },
+        );
+      },
+      title: 'Custom',
+      text: 'Custom Widget Alert',
+      leadingImage: 'assets/custom.gif',
+    );
+
     return Scaffold(
-      body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          children: [
-            // _button('Play video from File'),
-            _button(
-              'Play video from Network',
-              onPressed: () => Navigator.of(context).pushNamed('/fromNetwork'),
-            ),
-            _button(
-              'Play video from Youtube',
-              onPressed: () => Navigator.of(context).pushNamed('/fromYoutube'),
-            ),
-            _button(
-              'Play video from Network quality urls',
-              onPressed: () =>
-                  Navigator.of(context).pushNamed('/fromNetworkQualityUrls'),
-            ),
-            _button(
-              'Play video from Asset (with custom labels)',
-              onPressed: () => Navigator.of(context).pushNamed('/fromAsset'),
-            ),
-            _button(
-              'Play video from Vimeo',
-              onPressed: () => Navigator.of(context).pushNamed('/fromVimeoId'),
-            ),
-            _button(
-              'Play private video from Vimeo',
-              onPressed: () =>
-                  Navigator.of(context).pushNamed('/fromVimeoPrivateId'),
-            ),
-            _button(
-              'Custom Video player',
-              onPressed: () => Navigator.of(context).pushNamed('/customVideo'),
-            ),
-          ],
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 1,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: Text(
+          "QuickAlert Demo",
+          style: Theme.of(context).textTheme.titleMedium,
         ),
+      ),
+      body: ListView(
+        children: [
+          const SizedBox(height: 20),
+          successAlert,
+          const SizedBox(height: 20),
+          errorAlert,
+          const SizedBox(height: 20),
+          warningAlert,
+          const SizedBox(height: 20),
+          infoAlert,
+          const SizedBox(height: 20),
+          confirmAlert,
+          const SizedBox(height: 20),
+          loadingAlert,
+          const SizedBox(height: 20),
+          customAlert,
+          const SizedBox(height: 20),
+        ],
       ),
     );
   }
 
-  Widget _button(String text, {void Function()? onPressed}) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: OutlinedButton(
-          onPressed: onPressed ?? () {},
-          child: Text(
-            text,
-            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+  Card buildButton({
+    required onTap,
+    required title,
+    required text,
+    required leadingImage,
+  }) {
+    return Card(
+      shape: const StadiumBorder(),
+      margin: const EdgeInsets.symmetric(
+        horizontal: 20,
+      ),
+      clipBehavior: Clip.antiAlias,
+      elevation: 1,
+      child: ListTile(
+        onTap: onTap,
+        leading: CircleAvatar(
+          backgroundImage: AssetImage(
+            leadingImage,
           ),
+        ),
+        title: Text(title ?? ""),
+        subtitle: Text(text ?? ""),
+        trailing: const Icon(
+          Icons.keyboard_arrow_right_rounded,
         ),
       ),
     );
