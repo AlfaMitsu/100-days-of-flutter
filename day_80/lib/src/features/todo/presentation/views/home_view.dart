3e22ea58fe4485bbc8681f526904e8e43e972414
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-05-01 13:50:30 +0800

    day 80 update

--- day_80/lib/src/features/todo/presentation/views/home_view.dart
+++ day_80/lib/src/features/todo/presentation/views/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../constants/colors.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
@@ -15,20 +17,16 @@ class HomeView extends StatelessWidget {
               child: const Text('Show Awesome SnackBar'),
               onPressed: () {
                 final snackBar = SnackBar(
-                  /// need to set following properties for best effect of awesome_snackbar_content
                   elevation: 0,
                   behavior: SnackBarBehavior.floating,
-                  backgroundColor: Colors.transparent,
+                  backgroundColor: kTransparent,
                   content: AwesomeSnackbarContent(
                     title: 'On Snap!',
                     message:
                         'This is an example error message that will be shown in the body of snackbar!',
-
-                    /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
                     contentType: ContentType.failure,
                   ),
                 );
-
                 ScaffoldMessenger.of(context)
                   ..hideCurrentSnackBar()
                   ..showSnackBar(snackBar);
@@ -39,23 +37,18 @@ class HomeView extends StatelessWidget {
               child: const Text('Show Awesome Material Banner'),
               onPressed: () {
                 final materialBanner = MaterialBanner(
-                  /// need to set following properties for best effect of awesome_snackbar_content
                   elevation: 0,
-                  backgroundColor: Colors.transparent,
+                  backgroundColor: kTransparent,
                   forceActionsBelow: true,
                   content: AwesomeSnackbarContent(
                     title: 'Oh Hey!!',
                     message:
                         'This is an example error message that will be shown in the body of materialBanner!',
-
-                    /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
                     contentType: ContentType.success,
-                    // to configure for material banner
                     inMaterialBanner: true,
                   ),
                   actions: const [SizedBox.shrink()],
                 );
-
                 ScaffoldMessenger.of(context)
                   ..hideCurrentMaterialBanner()
                   ..showMaterialBanner(materialBanner);
